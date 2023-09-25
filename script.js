e9b31ecdcc3514912c1046bd2487d3a97a482b7f
Benaid-Rudan <benaid.rudan@edu.fit.ba>
2023-09-25 13:01:24 +0200

    Section project added + responsive edit

--- script.js
+++ script.js
@@ -1,18 +1,24 @@
  var typed = new Typed(".multiple-text", {
-     strings: ["IT Student", "Future Software Engineer"],
+     strings: ["IT Student"
+      , "Future Software Engineer"
+    ],
      typeSpeed:100,
-     backSpeed:100,
-     backDelay:1000,
      loop:true
  })
 
 var typed2 = new Typed(".omeni",{
-    strings: ["As a third-year student in the Faculty of Information Technologies, I am passionate about software development, programming and data analysis. I am excited to apply my knowledge and skills to real-world projects, and I am always looking for new opportunities to learn and grow."],
+    strings: ["Former amateur football player, currently a fourth-year student in the Faculty of Information Technologies. I am passionate about software development, programming and data analysis. I am excited to apply my knowledge and skills to real-world projects, and I am always looking for new opportunities to learn and grow."],
     typeSpeed:50,    
     loop:true
 })
 
 var typed3 = new Typed(".skilovi",{
+    strings: [" I have experience working in HTML, CSS, JavaScript which enables me to create visually appealing and interactive websites. Also, I'm familiar with C# and Angular and I'm eager to learn new technologies."],
+    typeSpeed:50,    
+    loop:true
+})
+
+var typed4 = new Typed(".projekts",{
     strings: [" I have experience working in HTML, CSS, JavaScript which enables me to create visually appealing and interactive websites. Also, I'm familiar with C# and Angular and I'm eager to learn new technologies."],
     typeSpeed:50,    
     loop:true
